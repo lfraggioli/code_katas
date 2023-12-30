b11e4738c54f09b1a59c1637da6c18ccd0db4f4b
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-12-30 17:10:16 -0300

    js practice

---
+++ JavaScript/calcAverage.js
@@ -0,0 +1,5 @@
+var find_average = (array) => {
+  return array.length === 0
+    ? 0
+    : array.reduce((acc, ind) => acc + ind, 0) / array.length;
+};
---
+++ JavaScript/reverseSeq.js
@@ -0,0 +1,7 @@
+const reverseCount = (n) => {
+  let array = [];
+  for (let i = n; i > 0; i--) {
+    array.push(i);
+  }
+  return array;
+};
