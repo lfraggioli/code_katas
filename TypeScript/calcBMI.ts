c1b15aa8ec74a9bfdf335f768e457bbf4aff3d2a
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-01-05 22:52:58 -0300

    night training

---
+++ TypeScript/calcBMI.ts
@@ -0,0 +1,13 @@
+export function bmi(weight: number, height: number): string {
+  let calc: number = weight / (height * height);
+
+  if (calc <= 18.5) {
+    return "Underweight";
+  } else if (calc <= 25.0) {
+    return "Normal";
+  } else if (calc <= 30.0) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
